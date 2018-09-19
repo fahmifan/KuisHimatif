78ab379064df7e884a408d40693c4289802682a3
Arif R Gilang <arif17003@mail.unpad.ac.id>
2018-09-19 07:18:11 +0700

    Tambahkan listener untuk trueButton

--- app/src/main/java/com/rz/kuishimatif/MainActivity.java
+++ app/src/main/java/com/rz/kuishimatif/MainActivity.java
@@ -2,6 +2,7 @@ package com.rz.kuishimatif;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         question = (TextView) findViewById(R.id.question);
         trueButton = (Button) findViewById(R.id.true_button);
         falseButton = (Button) findViewById(R.id.false_button);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
 }
