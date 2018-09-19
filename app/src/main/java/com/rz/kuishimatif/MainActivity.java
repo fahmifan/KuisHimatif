0e7d1b653d9d4edf7201862b84b5c677e91a7afe
Arif R Gilang <arif17003@mail.unpad.ac.id>
2018-09-19 07:16:46 +0700

    Declare dan sambungkan objek dari layout

--- app/src/main/java/com/rz/kuishimatif/MainActivity.java
+++ app/src/main/java/com/rz/kuishimatif/MainActivity.java
@@ -2,12 +2,23 @@ package com.rz.kuishimatif;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView question;
+    Button trueButton;
+    Button falseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        question = (TextView) findViewById(R.id.question);
+        trueButton = (Button) findViewById(R.id.true_button);
+        falseButton = (Button) findViewById(R.id.false_button);
+
     }
 }
